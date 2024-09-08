c0ca1ff541146195fad322913c2a41a16806cbba
tomasblaer <tomasblaer@gmail.com>
2024-09-08 19:16:30 +0000

    init

---
+++ src/main/java/com/hbv/baukur/BaukurApplication.java
@@ -0,0 +1,13 @@
+package com.hbv.baukur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaukurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaukurApplication.class, args);
+	}
+
+}
