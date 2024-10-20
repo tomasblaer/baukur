8b3660bee4c0c7a99c30699b402665eff6e15f5c
mikkimatt <mim23@hi.is>
2024-10-20 18:12:27 +0000

    Fix expenses for individual users

--- src/main/java/com/baukur/api/expenses/repository/ExpenseRepository.java
+++ src/main/java/com/baukur/api/expenses/repository/ExpenseRepository.java
@@ -4,7 +4,11 @@ import com.baukur.api.expenses.domain.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    List<Expense> findExpenseByCategoryIdAndUserId(Long categoryId, Long userId);
 
 }
