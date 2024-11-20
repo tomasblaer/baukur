87902b18b16de24083dfbda4887cb5a40df6d75d
valdisbjarna <valdisbriet22@gmail.com>
2024-11-20 23:07:15 +0000

    creating endpoint for expense graph

--- src/main/java/com/baukur/api/expenses/controller/ExpenseController.java
+++ src/main/java/com/baukur/api/expenses/controller/ExpenseController.java
@@ -1,15 +1,20 @@
 package com.baukur.api.expenses.controller;
 
 import com.baukur.api.expenses.domain.Expense;
+import com.baukur.api.expenses.domain.ExpenseAmountGraphResponse;
+import com.baukur.api.expenses.domain.GraphExpense;
 import com.baukur.api.expenses.service.ExpenseService;
 import com.baukur.api.user.domain.UserDetailsImpl;
+import org.hibernate.query.QueryParameter;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/expenses")
@@ -26,6 +31,21 @@ public class ExpenseController {
             return new ResponseEntity<>(expense, HttpStatus.OK);
         } catch (Exception e) {
             throw new RuntimeException("Failed to get expense");
+        }
+    }
+
+    @GetMapping("/graph")
+    public ResponseEntity<?> getExpensesForGraph(
+            @AuthenticationPrincipal UserDetailsImpl user,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date to)
+    {
+        try {
+            List<GraphExpense> expenses = expenseService.getExpensesForGraph(user, from, to);
+
+            return new ResponseEntity<>(expenses, HttpStatus.OK);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to get expenses");
         }
     }
 
@@ -58,8 +78,8 @@ public class ExpenseController {
 
     // Edit expense
     @PatchMapping
-    public ResponseEntity<?> editExpense(@RequestBody Expense expense) {
-        Expense editedExpense = expenseService.updateExpense(expense);
+    public ResponseEntity<?> editExpense(@RequestBody Expense expense, @AuthenticationPrincipal UserDetailsImpl user) {
+        Expense editedExpense = expenseService.updateExpense(expense, user);
         return new ResponseEntity<>(editedExpense, HttpStatus.OK);
     }
 
